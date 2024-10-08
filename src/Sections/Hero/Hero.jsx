8bc93ed17f4db03482305393294e315056734885
WafleraVertical <101136420+WafleraVertical@users.noreply.github.com>
2024-10-08 16:05:41 -0600

    Modificaciones en los Archivos
    SE cambian los iconos y parte de la sintaxis

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -2,8 +2,6 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
@@ -15,7 +13,6 @@ function Hero() {
   const {theme,toggleTheme}= useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -44,13 +41,11 @@ function Hero() {
         <h2>Data Analyst</h2>
         <h2>Data Engineer</h2>
         <span>
-          <a href="https://twitter.com" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon"/>
-          </a>
+
           <a href="https://github.com/WafleraVertical" target="_blank">
             <img src={githubIcon} alt="Github icon"/>
           </a>
-          <a href="www.linkedin.com/in/luis-eduardo-polaco-romero" target="_blank">
+          <a href="https://www.linkedin.com/in/luis-eduardo-polaco-romero" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon"/>
           </a>
         </span>
